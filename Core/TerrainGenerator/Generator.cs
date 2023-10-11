86abda872f0128bce7dc3674bcfd7b6f0f7dc1f6
Charles Zhang <szinubuntu@gmail.com>
2023-10-11 10:05:56 -0500

    [Generator] General Enhancement
    - Fix contour rendering logic
    - Update render options interpretation
    - Add map render types
    - Parallelize trivial purely functional algorithms

--- Core/TerrainGenerator/Generator.cs
+++ Core/TerrainGenerator/Generator.cs
@@ -7,13 +7,14 @@ namespace TerrainGenerator
     {
         Height,
         Relief,
-        Contour
+        Contour,
+        HeightWithContour
     }
-    public struct RenderConfiguration
+    public class RenderConfiguration
     {
-        public double SeaLevelRatio { get; set; }
-        public bool ShowSea { get; set; }
-        public double ContourLineDensity { get; set; }
+        public double SeaLevelRatio { get; set; } = 0.2;
+        public bool ShowSea { get; set; } = true;
+        public double ContourLineDensity { get; set; } = 25;
     }
     public record Resolution(int Rows, int Columns);
     public record Mesh(Resolution Resolution, Vector[] Vertices, Edge[] Edges, Face[] Faces)
@@ -91,29 +92,31 @@ namespace TerrainGenerator
         {
             // Initialize mesh
             Vector[][] mesh = new Vector[Resolution.Rows][];
-            for (int row = 0; row < Resolution.Rows; row++)
+            Parallel.For(0, Resolution.Rows, row =>
             {
                 mesh[row] = new Vector[Resolution.Columns];
                 for (int col = 0; col < Resolution.Columns; col++)
                     mesh[row][col] = new Vector(row, col, GridVertices[row][col]);
-            }
+            });
 
             // Convert 2D vertices into 1D array
             Vector[] vertices = new Vector[Resolution.Rows * Resolution.Columns];
-            for (int i = 0; i < Resolution.Rows * Resolution.Columns; i++)
+            Parallel.For(0, Resolution.Rows * Resolution.Columns, i =>
+            {
                 vertices[i] = mesh[i / Resolution.Columns][i % Resolution.Columns];
+            }); 
 
             // Generate triangulated faces
             int faceCount = (Resolution.Rows - 1) * (Resolution.Columns - 1) * 2;
             Face[] faces = new Face[faceCount];
-            for (int i = 0; i < faceCount / 2; i++)
+            Parallel.For(0, faceCount / 2, i =>
             {
                 int colOffset = i % (Resolution.Columns - 1);
                 int rowOffset = i / (Resolution.Columns - 1);
                 int totalOffset = colOffset + rowOffset * Resolution.Columns;
                 faces[i * 2] = new Face(0 + totalOffset, 1 + totalOffset, Resolution.Columns + totalOffset);
                 faces[i * 2 + 1] = new Face(1 + totalOffset, Resolution.Columns + 1 + totalOffset, Resolution.Columns + totalOffset);
-            }
+            });
 
             result = new Mesh(Resolution, vertices, Array.Empty<Edge>(), faces);
             return this;
@@ -134,6 +137,9 @@ namespace TerrainGenerator
                 case RenderType.Contour:
                     RenderContour(this, filePath, configurations);
                     break;
+                case RenderType.HeightWithContour:
+                    RenderHeightWithContour(this, filePath, configurations);
+                    break;
             }
             return this;
         }
@@ -144,12 +150,12 @@ namespace TerrainGenerator
         {
             // Initialize grid
             double[][] grid = new double[resolution.Rows][];
-            for (int row = 0; row < resolution.Rows; row++)
+            Parallel.For(0, resolution.Rows, row =>
             {
                 grid[row] = new double[resolution.Columns];
                 for (int col = 0; col < resolution.Columns; col++)
                     grid[row][col] = initialValue;
-            }
+            });
             return grid;
         }
         public static double[][] MultiLayerPertubate(FastNoiseLite noise, Resolution resolution, (float Frequency, double MaxHeight)[] levels)
@@ -158,36 +164,42 @@ namespace TerrainGenerator
             double[][] grid = InitializeGrid(resolution, initialValue: 0);
 
             // Pertubate
-            for (int row = 0; row < resolution.Rows; row++)
+            foreach (var level in levels)
             {
-                for (int col = 0; col < resolution.Columns; col++)
+                Parallel.For(0, resolution.Rows, row =>
                 {
-                    foreach (var level in levels)
+                    for (int col = 0; col < resolution.Columns; col++)
                     {
+
                         noise.SetFrequency(level.Frequency);
                         grid[row][col] += noise.GetNoise(row, col) * level.MaxHeight * 2 - level.MaxHeight;
                     }
-                }
+                });
             }
 
             return grid;
         }
         public static void CutoffInPlace(double[][] grid, double[][] cutOffGrid, Resolution resolution)
         {
-            for (int row = 0; row < resolution.Rows; row++)
+            Parallel.For(0, resolution.Rows, row =>
             {
                 for (int col = 0; col < resolution.Columns; col++)
                 {
                     double cutOffHeight = cutOffGrid[row][col];
                     grid[row][col] = grid[row][col] > cutOffHeight ? grid[row][col] : cutOffHeight;
                 }
-            }
+            });
         }
-        public static void RenderContour(Generator generation, string filePath, RenderConfiguration configurations)
+        public static void RenderHeightWithContour(Generator generation, string filePath, RenderConfiguration configurations)
         {
             double heightRange = generation.HeightRange;
             double minHeight = generation.MinHeight;
             double contourSpan = heightRange / configurations.ContourLineDensity;
+            double seaLevel = configurations.SeaLevelRatio * heightRange + minHeight;
+
+            Color counterLineMainColor = Color.FromArgb(36, 30, 29);
+            Color seaInterpColor = Color.FromArgb(61, 168, 204);
+            double spill = 0.1;
 
             Bitmap bmp = new(generation.Resolution.Columns, generation.Resolution.Rows);
             for (int row = 0; row < generation.Resolution.Rows; row++)
@@ -196,31 +208,74 @@ namespace TerrainGenerator
                 {
                     double height = generation.GetHeight(row, col);
                     double remainder = (height - minHeight) % contourSpan;
-                    if (Math.Abs(remainder - contourSpan) > contourSpan * 0.01)
-                        bmp.SetPixel(col, row, Color.FromArgb(36, 30, 29));
+                    if (configurations.ShowSea && height < seaLevel)
+                    {
+                        if (Math.Abs(remainder - contourSpan) < contourSpan * spill)
+                            bmp.SetPixel(col, row, Interpolate(seaInterpColor, counterLineMainColor, Math.Abs(remainder - contourSpan) / (contourSpan * spill)));
+                        else
+                            bmp.SetPixel(col, row, seaInterpColor);
+                    }
+                    else
+                    {
+                        double ratio = (height - minHeight) / heightRange;
+                        int value = (int)(ratio * 255);
+                        Color landInterpColor = Color.FromArgb(value, value, value);
+
+                        if (Math.Abs(remainder - contourSpan) < contourSpan * spill)
+                            bmp.SetPixel(col, row, Interpolate(landInterpColor, counterLineMainColor, Math.Abs(remainder - contourSpan) / (contourSpan * spill)));
+                        else
+                        {
+                            bmp.SetPixel(col, row, landInterpColor);
+                        }
+                    }
+                }
+            }
+            bmp.Save(Path.GetFullPath(filePath));
+        }
+        public static void RenderContour(Generator generation, string filePath, RenderConfiguration configurations)
+        {
+            double heightRange = generation.HeightRange;
+            double minHeight = generation.MinHeight;
+            double contourSpan = heightRange / configurations.ContourLineDensity;
+
+            Bitmap bmp = new(generation.Resolution.Columns, generation.Resolution.Rows);
+            Parallel.For(0, generation.Resolution.Rows, row =>
+            {
+                for (int col = 0; col < generation.Resolution.Columns; col++)
+                {
+                    double height = generation.GetHeight(row, col);
+                    double remainder = (height - minHeight) % contourSpan;
+                    if (Math.Abs(remainder - contourSpan) < contourSpan * 0.05)
+                        bmp.SetPixel(col, row, Interpolate(Color.FromArgb(36, 30, 29), Color.FromArgb(12, 12, 12), Math.Abs(remainder - contourSpan) / (contourSpan * 0.05)));
                     else
                         bmp.SetPixel(col, row, Color.FromArgb(224, 222, 222));
                 }
-            }
+            });
             bmp.Save(Path.GetFullPath(filePath));
         }
         public static void RenderHeightMap(Generator generation, string filePath, RenderConfiguration configurations)
         {
             double heightRange = generation.HeightRange;
             double minHeight = generation.MinHeight;
+            double seaLevel = configurations.SeaLevelRatio * heightRange + minHeight;
 
             Bitmap bmp = new(generation.Resolution.Columns, generation.Resolution.Rows);
-            for (int row = 0; row < generation.Resolution.Rows; row++)
+            Parallel.For(0, generation.Resolution.Rows, row =>
             {
                 for (int col = 0; col < generation.Resolution.Columns; col++)
                 {
                     double height = generation.GetHeight(row, col);
-                    double ratio = (height - minHeight) / heightRange;
-                    int value = (int)(ratio * 255);
-                    Color color = Color.FromArgb(value, value, value);
-                    bmp.SetPixel(col, row, color);
+                    if (configurations.ShowSea && height < seaLevel)
+                        bmp.SetPixel(col, row, Color.FromArgb(61, 168, 204));
+                    else
+                    {
+                        double ratio = (height - minHeight) / heightRange;
+                        int value = (int)(ratio * 255);
+                        Color color = Color.FromArgb(value, value, value);
+                        bmp.SetPixel(col, row, color);
+                    }
                 }
-            }
+            });
             bmp.Save(Path.GetFullPath(filePath));
         }
         public static void RenderRelief(Generator generation, string filePath, RenderConfiguration configurations)
@@ -239,7 +294,7 @@ namespace TerrainGenerator
             double seaRange = seaLevel - minHeight;
 
             Bitmap bmp = new(generation.Resolution.Columns, generation.Resolution.Rows);
-            for (int row = 0; row < generation.Resolution.Rows; row++)
+            Parallel.For(0, generation.Resolution.Rows, row =>
             {
                 for (int col = 0; col < generation.Resolution.Columns; col++)
                 {
@@ -251,17 +306,16 @@ namespace TerrainGenerator
                     else if (height < seaLevel)
                         bmp.SetPixel(col, row, Interpolate(lightSeaColor, darkSeaColor, (seaLevel - height) / seaRange)); // Sea
                 }
-            }
+            });
             bmp.Save(Path.GetFullPath(filePath));
-
-            static Color Interpolate(Color color1, Color color2, double ratio)
-            {
-                return Color.FromArgb(
-                    (int)((double)color1.R + ratio * (color2.R - color1.R)),
-                    (int)((double)color1.G + ratio * (color2.G - color1.G)),
-                    (int)((double)color1.B + ratio * (color2.B - color1.B))
-                );
-            }
+        }
+        public static Color Interpolate(Color color1, Color color2, double ratio)
+        {
+            return Color.FromArgb(
+                (int)((double)color1.R + ratio * (color2.R - color1.R)),
+                (int)((double)color1.G + ratio * (color2.G - color1.G)),
+                (int)((double)color1.B + ratio * (color2.B - color1.B))
+            );
         }
         #endregion
     }
